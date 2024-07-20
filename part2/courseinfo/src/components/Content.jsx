74fdfdc25654acc5a8d4bfafe79030415cee53bd
Alberto Ballesteros Rivas <ballesterosrivasa@gmail.com>
2024-07-21 00:28:49 +0200

    Ejercicio 2.2

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -4,6 +4,8 @@ const Content = ({ parts }) => {
   return (
     <>
     {parts.map(part => <Part part={part} key={part.id} />)}
+    
+    <p>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</p>
     </>
   );
 };
