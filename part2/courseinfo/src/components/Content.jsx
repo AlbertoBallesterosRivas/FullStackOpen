5c62598dbc9922e47e4d4d0b82389a0660dec9eb
Alberto Ballesteros Rivas <ballesterosrivasa@gmail.com>
2024-07-21 00:30:02 +0200

    Ejercicio 2.3

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,11 +1,12 @@
 import Part from './Part'
 
 const Content = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
   return (
     <>
     {parts.map(part => <Part part={part} key={part.id} />)}
     
-    <p>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</p>
+    <p>total of {total} exercises</p>
     </>
   );
 };
