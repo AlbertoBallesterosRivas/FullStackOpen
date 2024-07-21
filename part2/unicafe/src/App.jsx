3d876c8bedecadd8a9b802b7e35bbb67314cd0e6
Alberto Ballesteros Rivas <ballesterosrivasa@gmail.com>
2024-07-21 20:24:27 +0200

    Ejercicio 2.16

--- part2/unicafe/src/App.jsx
+++ part2/unicafe/src/App.jsx
@@ -3,12 +3,14 @@ import personService from "./services/persons";
 import Filter from "./components/Filter";
 import Form from "./components/Form";
 import Persons from "./components/Persons";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     personService.getAll().then((response) => {
@@ -27,7 +29,12 @@ const App = () => {
     if (!persons.some((person) => person.name == newName)) {
       personService.create(newPerson).then((response) => {
         setPersons(persons.concat(response.data));
-
+        setMessage(
+          `Added '${newPerson.name}'`
+        )
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
       });
     } else {
       if(window.confirm(
@@ -40,6 +47,12 @@ const App = () => {
             person.id === oldPerson.id ? { ...person, name: newName, number: newNumber, } : person
             )
           );
+          setMessage(
+            `'${newPerson.name}' edited`
+          )
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
         });
       }
 
@@ -68,6 +81,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} />
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
 
       <h2>add a new</h2>
