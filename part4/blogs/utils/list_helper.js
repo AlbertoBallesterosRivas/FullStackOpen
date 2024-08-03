832541d3dfbb2ceb8ab22a9f06439d80186b6a82
Alberto Ballesteros Rivas <ballesterosrivasa@gmail.com>
2024-08-04 00:47:45 +0200

    Ejercicio 4.5

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -8,7 +8,19 @@ const totalLikes = (blogs) => {
   }, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const favoriteBlog = blogs.reduce(function (prev, current) {
+    return prev.likes > current.likes ? prev : current;
+  }, blogs[0]);
+  return {
+    title: favoriteBlog.title,
+    author: favoriteBlog.author,
+    likes: favoriteBlog.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
