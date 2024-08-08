a3b000383448a12c40b05b24ee4ba963d076710c
Alberto Ballesteros Rivas <ballesterosrivasa@gmail.com>
2024-08-08 19:40:21 +0200

    Ejercicio 6.7

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,48 +1,37 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { createStore } from 'redux'
-import reducer from './reducers/anecdoteReducer'
-import { voteAnecdote, createAnecdote } from './reducers/anecdoteReducer'
+import { useSelector, useDispatch } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducers/anecdoteReducer";
+import { voteAnecdote, createAnecdote } from "./reducers/anecdoteReducer";
+import AnecdoteForm from "./components/AnecdoteForm"
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
 
   const vote = (id) => {
-    dispatch(voteAnecdote(id))
-  }
+    dispatch(voteAnecdote(id));
+  };
 
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
-  }
-
-  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
+  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes);
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {sortedAnecdotes.map(anecdote =>
+      {sortedAnecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
       <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name="anecdote" /></div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteForm />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
