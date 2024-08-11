c8a93af76a39281bb39ae4e5ae0d6737547121d9
Alberto Ballesteros Rivas <ballesterosrivasa@gmail.com>
2024-08-11 15:08:37 +0200

    Ejercicio 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,17 +2,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { createAnecdote, voteAnecdote } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
   const dispatch = useDispatch();
+  //const anecdotes = useSelector(state => state.anecdotes)
+
+  //useSelector(state => state.anecdotes.map((anecdote) => console.log("anecdote", anecdote)))
+
+  const anecdotes = useSelector((state) => state.anecdotes.filter((anecdote) => anecdote.content.toLowerCase().includes(state.filter.toLowerCase())));
+  
 
   const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes);
-
-  const addAnecdote = (event) => {
-    event.preventDefault();
-    const content = event.target.anecdote.value;
-    event.target.anecdote.value = "";
-    dispatch(createAnecdote(content));
-  };
 
   const vote = (id) => {
     dispatch(voteAnecdote(id));
